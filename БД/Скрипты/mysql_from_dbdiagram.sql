7f1b41de6b49b757e078cc92f845e4abf25258ec
arkhanghel <arkhanghel@mail.ru>
2021-06-09 18:34:05 +0300

    Обновление БД

--- БД/Скрипты/mysql_from_dbdiagram.sql
+++ БД/Скрипты/mysql_from_dbdiagram.sql
@@ -5,24 +5,39 @@ CREATE TABLE `user_role` (
 
 CREATE TABLE `user` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
+  `organization_id` int COMMENT 'on delete restrict, on update restrict',
   `name` varchar(50),
-  `company_name` varchar(50),
   `email` varchar(20),
-  `phone` varchar(12),
-  `avatar` blob(4200000),
-  `password` varchar(16),
-  `role` int COMMENT 'on delete restrict, on update restrict',
+  `password` varchar(18),
+  `role_id` int COMMENT 'on delete restrict, on update restrict',
   `reg_date` date
 );
 
-CREATE TABLE `product_status` (
+CREATE TABLE `organization` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50),
+  `about` text,
+  `email` varchar(20),
+  `phone` varchar(12),
+  `avatar` blob(4200000),
+  `reg_date` date
+);
+
+CREATE TABLE `measure_unit` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(30)
 );
 
-CREATE TABLE `measure_unit_type` (
+CREATE TABLE `balance` (
+  `product_id` int,
+  `quantity` int
+);
+
+CREATE TABLE `operations` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(30)
+  `user_id` int COMMENT 'on delete set null, on update set null',
+  `product_id` int COMMENT 'on delete cascade, on update cascade',
+  `quantity` int
 );
 
 CREATE TABLE `product` (
@@ -30,12 +45,11 @@ CREATE TABLE `product` (
   `seller_id` int COMMENT 'on delete restrict, on update restrict',
   `name` varchar(50),
   `about` text,
-  `status` int,
   `img` blob(4200000),
   `added_on` date,
   `modified_on` date,
   `price` int,
-  `measure_unit` int COMMENT 'on delete restrict, on update restrict'
+  `measure_unit_id` int COMMENT 'on delete restrict, on update restrict'
 );
 
 CREATE TABLE `category` (
@@ -60,7 +74,7 @@ CREATE TABLE `buy_order` (
   `sent_on` date,
   `deliver_date` date,
   `deliver_address` varchar(50),
-  `status` int COMMENT 'on delete restrict, on update restrict',
+  `status_id` int COMMENT 'on delete restrict, on update restrict',
   `order_sum` int
 );
 
@@ -75,30 +89,42 @@ CREATE TABLE `order_content` (
   `pos_sum` int
 );
 
-ALTER TABLE `user` ADD FOREIGN KEY (`role`) REFERENCES `user_role` (`id`);
+ALTER TABLE `user` ADD FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`);
 
-ALTER TABLE `product` ADD FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`);
+ALTER TABLE `user` ADD FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`);
 
-ALTER TABLE `product` ADD FOREIGN KEY (`status`) REFERENCES `product_status` (`id`);
+ALTER TABLE `balance` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
-ALTER TABLE `product` ADD FOREIGN KEY (`measure_unit`) REFERENCES `measure_unit_type` (`id`);
+ALTER TABLE `operations` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `operations` ADD FOREIGN KEY (`product_id`) REFERENCES `balance` (`product_id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`seller_id`) REFERENCES `organization` (`id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`measure_unit_id`) REFERENCES `measure_unit` (`id`);
 
 ALTER TABLE `category_content` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 ALTER TABLE `category_content` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
-ALTER TABLE `buy_order` ADD FOREIGN KEY (`buyer_id`) REFERENCES `user` (`id`);
+ALTER TABLE `buy_order` ADD FOREIGN KEY (`buyer_id`) REFERENCES `organization` (`id`);
 
-ALTER TABLE `buy_order` ADD FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`);
+ALTER TABLE `buy_order` ADD FOREIGN KEY (`seller_id`) REFERENCES `organization` (`id`);
 
-ALTER TABLE `buy_order` ADD FOREIGN KEY (`status`) REFERENCES `order_status` (`id`);
+ALTER TABLE `buy_order` ADD FOREIGN KEY (`status_id`) REFERENCES `order_status` (`id`);
 
 ALTER TABLE `order_content` ADD FOREIGN KEY (`order_id`) REFERENCES `buy_order` (`id`);
 
 ALTER TABLE `order_content` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
-CREATE INDEX `user_index_0` ON `user` (`name`);
+CREATE UNIQUE INDEX `user_index_0` ON `user` (`id`, `organization_id`);
 
-CREATE INDEX `user_index_1` ON `user` (`company_name`);
+CREATE INDEX `organization_index_1` ON `organization` (`name`);
 
-CREATE INDEX `product_index_2` ON `product` (`name`);
+CREATE UNIQUE INDEX `balance_index_2` ON `balance` (`product_id`);
+
+CREATE INDEX `product_index_3` ON `product` (`name`);
+
+CREATE UNIQUE INDEX `category_content_index_4` ON `category_content` (`category_id`, `product_id`);
+
+CREATE UNIQUE INDEX `order_content_index_5` ON `order_content` (`order_id`, `product_id`);
